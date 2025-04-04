ec5c7c5d439a94ae29e3c54c3e9b5f8cba234870
OmarAraby <omararaby789@gmail.com>
2025-04-05 00:44:40 +0200

    add invoice number to Order model & handle some stats in user order in order controller

--- src/Modules/Orders/order.routes.js
+++ src/Modules/Orders/order.routes.js
@@ -4,31 +4,6 @@ const {createOrder,cancelOrder,getAllOrder,getUserOrders} = require("./order.con
 const validateSchema = require("../../utils/validation/validateSchema");
 const { createOrderValidation } = require("./order.schema");
 
-// Debug middleware with error logging
-// const debugMiddleware = (req, res, next) => {
-//   try {
-//     console.log('=== Debug Information ===');
-//     console.log('Request Body:', JSON.stringify(req.body, null, 2));
-//     console.log('User:', req.user);
-//     console.log('Headers:', req.headers);
-//     console.log('=== End Debug Info ===');
-//     next();
-//   } catch (error) {
-//     console.error('Debug Middleware Error:', error);
-//     next(error);
-//   }
-// };
-
-// // Add error logging middleware
-// const errorLogger = (err, req, res, next) => {
-//   console.error('Error Details:', {
-//     message: err.message,
-//     stack: err.stack,
-//     body: req.body,
-//     user: req.user
-//   });
-//   next(err);
-// };
 
 router.use(protectionMW);
 
