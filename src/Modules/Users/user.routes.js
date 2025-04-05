1cc2e87b9cec9d2285c0558bc55a65c382b17014
OmarAraby <omararaby789@gmail.com>
2025-04-05 06:56:47 +0200

    make endpoint for deleting profile iamge

--- src/Modules/Users/user.routes.js
+++ src/Modules/Users/user.routes.js
@@ -1,7 +1,7 @@
 // Defines API routes for Users and connects them to the Controller
 
 const router = require('express').Router();
-const {registerUser, loginUser,updateUserProfile,getAllUsers, forgotPassword, resetPassword, getUserById} = require('./user.controller');
+const {registerUser, loginUser,updateUserProfile,getAllUsers, forgotPassword, resetPassword, getUserById,deleteProfileImage} = require('./user.controller');
 const validateSchema = require('../../utils/validation/validateSchema');
 const {  protectionMW } = require('../../middlewares/authMiddleware');
 const { userSchemaValidation, userUpdateSchemaValidation, forgotPassSchema, resetPassSchema } = require('./user.schema');
@@ -20,8 +20,8 @@ router.put('/update-profile',
 );
 router.get('/users', getAllUsers);
 router.post('/forgot-password',validateSchema(forgotPassSchema),forgotPassword);
+router.delete('/profile-image',protectionMW,deleteProfileImage);
 router.post('/reset-password/:token', validateSchema(resetPassSchema), resetPassword);
 router.get('/users/:id', getUserById);  
-
 
 module.exports = router;
