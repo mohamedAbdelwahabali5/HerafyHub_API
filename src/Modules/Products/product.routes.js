f95a619e62353d14308fb147670a6852a2eecf2f
mohamedAbdelwahabali5 <mohamedabdelwahabali5@gmail.com>
2025-04-11 14:21:33 +0200

    Update product.routes.js

--- src/Modules/Products/product.routes.js
+++ src/Modules/Products/product.routes.js
@@ -12,7 +12,7 @@ const { protectionMW, roleMiddleware } = require("../../middlewares/authMiddlewa
 // Place specific routes before parameter routes
 router.post("/",roleMiddleware, productController.createProduct);
 router.get("/",protectionMW, productController.getProductsCategory);
-router.get("/all", protectionMW,productController.getAllProducts);
+router.get("/all",productController.getAllProducts);
 router.get("/search", protectionMW,productController.searchProductsByTitleInCategory);
 router.post("/all", protectionMW,productController.insertManyProducts);
 router.get("/categories/count",protectionMW, productController.countProductsByCategory);
